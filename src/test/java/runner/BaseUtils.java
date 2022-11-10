09d74f10558acf9ed4e3cbe958f71b7debfb1c56
Anna Fedorova <snysmymric@gmail.com>
2022-11-09 21:44:52 -0800

    updated baseUtils (#2)

--- src/test/java/runner/BaseUtils.java
+++ src/test/java/runner/BaseUtils.java
@@ -6,11 +6,13 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
 public class BaseUtils {
     static WebDriver createDriver() {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         return driver;
     }
 }
